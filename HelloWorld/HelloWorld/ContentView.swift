15f3a97a5dd3a5a591c8a6d96f92cc20fa2b2917
jaeryo2357 <jaeryo2357@naver.com>
2019-12-14 13:46:48 +0900

    create Helloworld project

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by YangMinUk on 14/12/2019.
+//  Copyright © 2019 MinUkYang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
