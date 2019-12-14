0c91700db452718815e81d5719d0b4cd996df1cb
jaeryo2357 <jaeryo2357@naver.com>
2019-12-14 14:50:42 +0900

    create AutoLayout

---
+++ Chris_AutoLayout/Chris_AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chris_AutoLayout
+//
+//  Created by YangMinUk on 14/12/2019.
+//  Copyright © 2019 MinUkYang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
